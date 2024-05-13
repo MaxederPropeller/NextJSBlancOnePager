161df1709404d4ca39a8fa5679319b950822f773
maxederpropeller <max.schweller@gmail.com>
2024-05-13 17:04:41 +0200

    GO

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,14 @@
 
 
 
-import SectionAbout from "./components/about/AboutSection";
-
-import SectionGalerie from "./components/galerie/GalerieSection";
-import SectionHero from "./components/hero/HeroSection";
-import SectionKontakt from "./components/contact/KontaktSection";
-import SectionAngebotTwo from "./components/angebot/AngebotSectionTwo";
-import { Suspense } from "react";
-import LoadingSpinner from "./loading";
-
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-24 overflow-x-hidden justify-center items-center">
 
-       <SectionHero />
-     <div className="max-w-7xl flex flex-col gap-24">
-        <SectionAbout />
+
       
-        <SectionAngebotTwo />
-        <Suspense fallback={<LoadingSpinner />}>
-          <SectionGalerie />
-        </Suspense>
-        <SectionKontakt />
-      
-     </div>
+
 
 
     </main>
